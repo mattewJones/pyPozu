a5b9ee41a1f6dac9f7841497fe5c6d1db59da99d
Matthew Jones <jaouen.hoel@gmail.com>
2022-05-24 10:43:22 +0200

    finished classifier

--- calc_features.py
+++ calc_features.py
@@ -1,2 +1,2 @@
-def calc_features(img):
+def calc_feature_vector(img):
 	return([1,2,3])
--- classifier.py
+++ classifier.py
@@ -1,12 +1,12 @@
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix,precision_recall_fscore_support
 from load_db import learn_data,eval_data
-from calc_features import calc_features 
+from calc_features import calc_feature_vector
 import numpy as np
 
 
 
-def calc_features(dataset_dict):
+def calc_feature_array(dataset_dict):
 	"""
 	transforme les données d'échantillons indexées par
 	classes dans dataset_dict en un tableau de valeurs d'attributs
@@ -17,7 +17,7 @@ def calc_features(dataset_dict):
 	resLabels=[]
 	for label,sampleList in dataset_dict.items():
 		for sample in sampleList :
-			feature=calc_features(sample.imData())
+			feature=calc_feature_vector(sample.imData())
 			resFeatures.append(feature)
 			resLabels.append(label)
 	return(np.array(resFeatures),np.array(resLabels))
@@ -26,8 +26,8 @@ def calc_features(dataset_dict):
 
 
 
-learn_features,learn_labels=calc_features(learn_data)
-eval_features,eval_labels=calc_features(eval_data)
+learn_features,learn_labels=calc_feature_array(learn_data)
+eval_features,eval_labels=calc_feature_array(eval_data)
 
 clf = KNeighborsClassifier(p=1)
 clf.fit(learn_features,learn_labels)
@@ -35,6 +35,29 @@ predicted_labels=clf.predict(eval_features)
 
 print(predicted_labels)
 
+labels=list(learn_data.keys())
+
+print(
+	confusion_matrix(
+		y_true=eval_labels,
+		y_pred=predicted_labels,
+		labels=labels
+		)
+	)
 
 
-print(confusion_matrix(y_true=eval_labels,y_pred=predicted_labels))
+print(
+	np.array(precision_recall_fscore_support(
+		y_true=eval_labels,
+		y_pred=predicted_labels,
+		labels=labels
+		)
+	)
+)
+
+
+
+
+print(labels)
+
+
